2137ababd9a8a35f7fcbbd7ee5be1c990e2151fb
Mistralys <s.mordziol@gmail.com>
2025-01-13 18:52:59 +0100

    Optional subjects.

--- src/global-functions.php
+++ src/global-functions.php
@@ -9,11 +9,11 @@ use AppUtils\Interfaces\StringableInterface;
 /**
  * Creates a new operation result.
  *
- * @param object $subject
+ * @param object|NULL $subject The origin of the result message to retrieve again later. If NULL, an empty `stdClass` object will be used.
  * @param string|StringableInterface|NULL $label
  * @return OperationResult
  */
-function operationResult(object $subject, $label=null) : OperationResult
+function operationResult(?object $subject=null, $label=null) : OperationResult
 {
     return new OperationResult($subject, $label);
 }
@@ -21,11 +21,11 @@ function operationResult(object $subject, $label=null) : OperationResult
 /**
  * Creates a new operation result collection.
  *
- * @param object $subject
+ * @param object|NULL $subject The origin of the result message to retrieve again later. If NULL, an empty `stdClass` object will be used.
  * @param string|StringableInterface|NULL $label
  * @return OperationResult_Collection
  */
-function operationCollection(object $subject, $label=null) : OperationResult_Collection
+function operationCollection(?object $subject=null, $label=null) : OperationResult_Collection
 {
     return new OperationResult_Collection($subject, $label);
 }
